aba99dc5651ded03d53bcd7b47d164d1cab05100
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-07 23:49:27 +0300

    added empty string condition

--- is_substring.py
+++ is_substring.py
@@ -1,4 +1,7 @@
 def is_substring(string: str, substring: str) -> bool:
+    if substring == "":
+        return True
+    
     for i in range(len(string) - len(substring) + 1):
         for j in range(len(substring)):
             if string[i + j] != substring[j]:
@@ -11,3 +14,4 @@ def is_substring(string: str, substring: str) -> bool:
 
 print(is_substring("12345", "234"))
 print(is_substring("12345", "235"))
+print(is_substring("123", ""))
