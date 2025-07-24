09a776dda2157fa7cbb3943c273e2306c08d5269
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-24 17:58:10 +0300

    task 3.3 in topic "working with files" done

--- working_with_files/working_with_files.py
+++ working_with_files/working_with_files.py
@@ -1,10 +1,12 @@
 # 3.1.
 import random
 
-for i in range(1, 11):
-    with open(f"working_with_files/{i}.txt", "wt") as file:
-        for j in range(3):
-            file.write(f"{random.randint(1, 999)}\n")
+
+def create_files():
+    for i in range(1, 11):
+        with open(f"working_with_files/{i}.txt", "wt") as file:
+            for j in range(3):
+                file.write(f"{random.randint(1, 999)}\n")
 
 
 # 3.2.
@@ -27,8 +29,46 @@ def sum_six_numbers(nums: list[int], path: str):
                 return f"Файл {path_to_file} испорчен"
     return sm
 
+def sum_six_numbers_main():
+    nums = []
+    for _ in range(2):
+        nums.append(random.randint(1, 10))
+    print(sum_six_numbers(nums, "working_with_files/"))
 
-nums = []
-for _ in range(2):
-    nums.append(random.randint(1, 10))
-print(sum_six_numbers(nums, "working_with_files/"))
+
+# 3.3.
+
+
+def create_cats():
+    class Cat:
+        def __init__(self, name, weight, freq):
+            self.name = name
+            self.weight = weight
+            self.freq = freq
+
+    path = "working_with_files/cats.txt"
+    with open(path, "rt", encoding="UTF-8") as f:
+        s = f.readline()
+        while s != "":
+            attrs = s.split(" ")
+            try:
+                for i in range(len(attrs)):
+                    if i == 0:
+                        name = str(attrs[i])
+                    if i == 1:
+                        weight = float(attrs[i])
+                        if weight <= 0 or weight >= 50:
+                            raise ValueError
+                    if i == 2:
+                        freq = int(attrs[i])
+                        if freq <= 0 or freq >= 1000:
+                            raise ValueError
+                assert i == 2
+                cat = Cat(name, weight, freq)
+                print(cat.name, cat.weight, cat.freq)
+            except AssertionError:
+                print("Количество атрибутов не равно 3")
+            except ValueError:
+                print("Заданы некорректные параметры")
+
+            s = f.readline()
