66931edc991c8f2c3f93325ec986a3c443aa26e0
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-24 17:38:55 +0300

    task 3.2 in topic "working with files" done

--- working_with_files/working_with_files.py
+++ working_with_files/working_with_files.py
@@ -2,6 +2,33 @@
 import random
 
 for i in range(1, 11):
-    with open(f"working_with_files/{i}.txt", "w") as file:
+    with open(f"working_with_files/{i}.txt", "wt") as file:
         for j in range(3):
             file.write(f"{random.randint(1, 999)}\n")
+
+
+# 3.2.
+import random
+
+
+def sum_six_numbers(nums: list[int], path: str):
+    sm = 0
+    for i in range(len(nums)):
+        path_to_file = f"{path}{nums[i]}.txt"
+        with open(path_to_file, "rt") as f:
+            try:
+                for j in range(3):
+                    s = f.readline().rstrip()
+                    sm += int(s)
+                assert j == 2
+            except AssertionError:
+                return f"Длина файла {path_to_file} не соответствует требуемой"
+            except ValueError:
+                return f"Файл {path_to_file} испорчен"
+    return sm
+
+
+nums = []
+for _ in range(2):
+    nums.append(random.randint(1, 10))
+print(sum_six_numbers(nums, "working_with_files/"))
