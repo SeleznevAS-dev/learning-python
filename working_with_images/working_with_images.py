dab33040d387fbf6530cf91642e3b36995d114ce
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-28 23:18:06 +0300

    task 3.2 in topic "processing images" done

--- working_with_images/working_with_images.py
+++ working_with_images/working_with_images.py
@@ -13,3 +13,39 @@ def image_converter(ext1, ext2):
         if im.mode == "RGBA":
             im = im.convert("RGB")
         im.save(f"{file.split('.')[0]}{ext2}")
+
+
+# 3.2.
+from glob import glob
+import os
+from PIL import Image, ImageDraw
+
+
+def image_converter_modified(ext1, ext2):
+    root_dir = os.path.join(os.getcwd(), "working_with_images")
+    founded_files = glob(os.path.join(root_dir, f"*{ext1}"))
+    for file in founded_files:
+        im = Image.open(file)
+        if im.mode == "RGBA":
+            im = im.convert("RGB")
+        sz = im.size
+        draw = ImageDraw.Draw(im)
+
+        draw.rectangle(
+            [
+                sz[0] // 2 - sz[1] // 8,
+                sz[1] // 2 - sz[1] // 8,
+                sz[0] // 2 + sz[1] // 8,
+                sz[1] // 2 + sz[1] // 8,
+            ],
+            outline=(0, 0, 0),
+            width=10,
+        )
+        draw.multiline_text(
+            (sz[0] // 2 - sz[1] // 16, sz[1] // 2 - sz[1] // 16),
+            "Hello,\nWorld!",
+            fill=(0, 0, 0),
+            font_size=50,
+        )
+        im.save(f"{file.split('.')[0]}{ext2}")
+        del draw
