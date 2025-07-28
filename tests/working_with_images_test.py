c8fc296104c516b44e0ff5cd6de26dcbaa656c6f
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-28 23:21:31 +0300

    added tests to "processing images" topic

---
+++ tests/working_with_images_test.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+from working_with_images.working_with_images import image_converter
+from PIL import Image
+
+
+class WorkingWithFilesTests(unittest.TestCase):
+    # 3.1.
+    def test_regression_create_files(self):
+        test_dir = os.path.join(os.getcwd(), "working_with_images")
+
+        test_png_path = os.path.join(test_dir, "test_image.png")
+        test_jpg_path = os.path.join(test_dir, "test_image.jpg")
+
+        try:
+            img = Image.new("RGB", (100, 100), color=(100, 100, 100))
+            img.save(test_png_path)
+
+            image_converter(".png", ".jpg")
+
+            self.assertTrue(os.path.exists(test_jpg_path))
+
+        finally:
+            if os.path.exists(test_png_path):
+                os.remove(test_png_path)
+            if os.path.exists(test_jpg_path):
+                os.remove(test_jpg_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
