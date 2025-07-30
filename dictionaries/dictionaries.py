350b05ae20b4ec52e3f03eb692364a3abd826699
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-30 23:29:32 +0300

    uncommented task 2

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -19,22 +19,22 @@ for key in list(dictionary.keys()):
     del dictionary[key]
 
 # 2.
-# import random
+import random
 
-# arr = []
-# for i in range(100):
-#     arr.append(random.randint(1, 10))
+arr = []
+for i in range(100):
+    arr.append(random.randint(1, 10))
 
 
-# def n_values(arr, n):
-#     dct = {}
-#     for i in arr:
-#         if i not in dct:
-#             dct[i] = 0
-#         else:
-#             dct[i] += 1
-#     ans = []
-#     for key, value in dct.items():
-#         if value == n:
-#             ans.append(key)
-#     return ans
+def n_values(arr, n):
+    dct = {}
+    for i in arr:
+        if i not in dct:
+            dct[i] = 0
+        else:
+            dct[i] += 1
+    ans = []
+    for key, value in dct.items():
+        if value == n:
+            ans.append(key)
+    return ans
