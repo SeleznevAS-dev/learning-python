dcc6abdbd020a2efdbb1e9d9ca72d6b8c3e2fb69
SeleznevAS-dev <fizika10a2@yandex.ru>
2025-07-30 23:23:34 +0300

    task 2 dictionaries done

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -1,17 +1,38 @@
 # 1.
+# import random
+
+# keys = []
+# for i in range(100):
+#     keys.append(random.randint(1, 100))
+
+# dictionary = {}
+# for i in range(len(keys)):
+#     index_str = str(i)
+#     dictionary[keys[i]] = index_str
+
+# for i in dictionary.keys():
+#     print(dictionary[i])
+
+# for key in list(dictionary.keys()):
+#     del dictionary[key]
+
+# 2.
 import random
 
-keys = []
+arr = []
 for i in range(100):
-    keys.append(random.randint(1, 100))
+    arr.append(random.randint(1, 10))
 
-dictionary = {}
-for i in range(len(keys)):
-    index_str = str(i)
-    dictionary[keys[i]] = index_str
 
-for i in dictionary.keys():
-    print(dictionary[i])
-
-for key in list(dictionary.keys()):
-    del dictionary[key]
+def n_values(arr, n):
+    dct = {}
+    for i in arr:
+        if i not in dct:
+            dct[i] = 0
+        else:
+            dct[i] += 1
+    ans = []
+    for key, value in dct.items():
+        if value == n:
+            ans.append(key)
+    return ans
